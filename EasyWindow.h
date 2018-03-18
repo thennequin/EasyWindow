8652211e453876a82a41fb1a3720aaa3bb9d6f31
CameleonTH <cameleonth@gmail.com>
2018-03-18 22:06:35 +0100

    Expose caller classes

--- EasyWindow.h
+++ EasyWindow.h
@@ -4,47 +4,141 @@
 class EasyWindow
 {
 public:
-	//Callbacks
+	//Caller
+	//No arg
+	template<typename R>
+	class Caller
+	{
+	public:
+		virtual R					Call() = 0;
+	};
 
+	template<typename R>
+	class FunctionCaller : public Caller<R>
+	{
+	public:
+		FunctionCaller(R(*pFunctionPtr)())
+		{
+			m_pFunctionPtr = pFunctionPtr;
+		}
+		virtual R					Call()
+		{
+			return m_pFunctionPtr();
+		}
+		R(*m_pFunctionPtr)();
+	};
+
+	template<typename C, typename R>
+	class InstanceCaller : public Caller<R>
+	{
+	public:
+		InstanceCaller(C* pThis, R(C::*pMemberPtr)())
+		{
+			m_pThis = pThis;
+			m_pMemberPtr = pMemberPtr;
+		}
+		virtual R					Call()
+		{
+			return (m_pThis->*m_pMemberPtr)();
+		}
+		R(C::*m_pMemberPtr)();
+		C* m_pThis;
+	};
+
+	//One args
+	template<typename T1, typename R>
+	class CallerOneArg
+	{
+	public:
+		virtual R					Call(const T1& oT1) = 0;
+	};
+
+	template<typename T1, typename R>
+	class FunctionCallerOneArg : public CallerOneArg<T1, R>
+	{
+	public:
+		FunctionCallerOneArg(R(*pFunctionPtr)(const T1&))
+		{
+			m_pFunctionPtr = pFunctionPtr;
+		}
+		virtual R					Call(const T1& oT1)
+		{
+			return m_pFunctionPtr(oT1);
+		}
+		R(*m_pFunctionPtr)(T1);
+	};
+
+	template<typename C, typename T1, typename R>
+	class InstanceCallerOneArg : public CallerOneArg<T1, R>
+	{
+	public:
+		InstanceCallerOneArg(C* pThis, R(C::*pMemberPtr)(T1))
+		{
+			m_pThis = pThis;
+			m_pMemberPtr = pMemberPtr;
+			m_pMemberPtrConstRef = NULL;
+		}
+		InstanceCallerOneArg(C* pThis, R(C::*pMemberPtrConstRef)(const T1&))
+		{
+			m_pThis = pThis;
+			m_pMemberPtr = NULL;
+			m_pMemberPtrConstRef = pMemberPtrConstRef;
+		}
+		virtual R					Call(const T1& oT1)
+		{
+			if (m_pMemberPtr != NULL)
+				return (m_pThis->*m_pMemberPtr)(oT1);
+			return (m_pThis->*m_pMemberPtrConstRef)(oT1);
+		}
+		R(C::*m_pMemberPtr)(T1);
+		R(C::*m_pMemberPtrConstRef)(const T1&);
+		C* m_pThis;
+	};
+
+	//Two args
+	template<typename T1, typename T2, typename R>
+	class CallerTwoArg
+	{
+	public:
+		virtual R					Call(const T1& oT1, const T2& oT2) = 0;
+	};
+
+	template<typename T1, typename T2, typename R>
+	class FunctionCallerTwoArg : public CallerTwoArg<T1, T2, R>
+	{
+	public:
+		FunctionCallerTwoArg(R(*pFunctionPtr)(T1, T2))
+		{
+			m_pFunctionPtr = pFunctionPtr;
+		}
+		virtual R					Call(const T1& oT1, const T2& oT2)
+		{
+			return m_pFunctionPtr(oT1, oT2);
+		}
+		R(*m_pFunctionPtr)(T1, T2);
+	};
+
+	template<typename C, typename T1, typename T2, typename R>
+	class InstanceCallerTwoArg : public CallerTwoArg<T1, T2, R>
+	{
+	public:
+		InstanceCallerTwoArg(C* pThis, R(C::*pMemberPtr)(T1, T2))
+		{
+			m_pThis = pThis;
+			m_pMemberPtr = pMemberPtr;
+		}
+		virtual R					Call(const T1& oT1, const T2& oT2)
+		{
+			return (m_pThis->*m_pMemberPtr)(oT1, oT2);
+		}
+		R(C::*m_pMemberPtr)(T1, T2);
+		C* m_pThis;
+	};
+
+	//Callbacks
 	template<typename R>
 	class Callback
 	{
-		class Caller
-		{
-		public:
-			virtual R					Call() = 0;
-		};
-
-		class FunctionCall : public Caller
-		{
-		public:
-			FunctionCall(R(*pFunctionPtr)())
-			{
-				m_pFunctionPtr = pFunctionPtr;
-			}
-			virtual R					Call()
-			{
-				return m_pFunctionPtr();
-			}
-			R(*m_pFunctionPtr)();
-		};
-
-		template<typename C>
-		class InstanceCall : public Caller
-		{
-		public:
-			InstanceCall(C* pThis, R(C::*pMemberPtr)())
-			{
-				m_pThis = pThis;
-				m_pMemberPtr = pMemberPtr;
-			}
-			virtual R					Call()
-			{
-				return (m_pThis->*m_pMemberPtr)();
-			}
-			R(C::*m_pMemberPtr)();
-			C* m_pThis;
-		};
 	public:
 		Callback()
 		{
@@ -57,14 +151,14 @@ public:
 		void							Set(R(*pFuncPtr)())
 		{
 			if (0 != m_pCaller) delete m_pCaller;
-			m_pCaller = new FunctionCall(pFuncPtr);
+			m_pCaller = new FunctionCaller<R>(pFuncPtr);
 		}
 
 		template<typename C>
 		void							Set(C* pThis, R(C::*pMemberPtr)())
 		{
 			if (0 != m_pCaller) delete m_pCaller;
-			m_pCaller = new InstanceCall<C>(pThis, pMemberPtr);
+			m_pCaller = new InstanceCaller<C, R>(pThis, pMemberPtr);
 		}
 
 		R								operator()()
@@ -73,65 +167,12 @@ public:
 			return R();
 		}
 	protected:
-		Caller*							m_pCaller;
+		Caller<R>*						m_pCaller;
 	};
 
 	template<typename T1, typename R>
 	class CallbackOneArg
 	{
-		class Caller
-		{
-		public:
-			virtual R					Call(const T1& oT1) = 0;
-		};
-
-		class FunctionCall : public Caller
-		{
-		public:
-			FunctionCall(R(*pFunctionPtr)(const T1&))
-			{
-				m_pFunctionPtr = pFunctionPtr;
-			}
-			virtual R					Call(const T1& oT1)
-			{
-				return m_pFunctionPtr(oT1);
-			}
-			R(*m_pFunctionPtr)(const T1&);
-		};
-
-		template<typename C>
-		class InstanceCall : public Caller
-		{
-		public:
-			InstanceCall(C* pThis, R(C::*pMemberPtr)(T1))
-			{
-				m_pThis = pThis;
-				m_pMemberPtr = pMemberPtr;
-			}
-			virtual R					Call(const T1& oT1)
-			{
-				return (m_pThis->*m_pMemberPtr)(oT1);
-			}
-			R(C::*m_pMemberPtr)(T1);
-			C* m_pThis;
-		};
-
-		template<typename C>
-		class InstanceCallConstRef : public Caller
-		{
-		public:
-			InstanceCallConstRef(C* pThis, R(C::*pMemberPtr)(const T1&))
-			{
-				m_pThis = pThis;
-				m_pMemberPtr = pMemberPtr;
-			}
-			virtual R					Call(const T1& oT1)
-			{
-				return (m_pThis->*m_pMemberPtr)(oT1);
-			}
-			R(C::*m_pMemberPtr)(const T1&);
-			C* m_pThis;
-		};
 	public:
 		CallbackOneArg()
 		{
@@ -144,21 +185,21 @@ public:
 		void							Set(R(*pFuncPtr)(T1))
 		{
 			if (0 != m_pCaller) delete m_pCaller;
-			m_pCaller = new FunctionCall(pFuncPtr);
+			m_pCaller = new FunctionCallerOneArg<T1, R>(pFuncPtr);
 		}
 
 		template<typename C>
 		void							Set(C* pThis, R(C::*pMemberPtr)(T1))
 		{
 			if (NULL != m_pCaller) delete m_pCaller;
-			m_pCaller = new InstanceCall<C>(pThis, pMemberPtr);
+			m_pCaller = new InstanceCallerOneArg<C, T1, R>(pThis, pMemberPtr);
 		}
 
 		template<typename C>
-		void							Set(C* pThis, R(C::*pMemberPtr)(const T1&))
+		void							Set(C* pThis, R(C::*pMemberPtrConstRef)(const T1&))
 		{
 			if (NULL != m_pCaller) delete m_pCaller;
-			m_pCaller = new InstanceCallConstRef<C>(pThis, pMemberPtr);
+			m_pCaller = new InstanceCallerOneArg<C, T1, R>(pThis, pMemberPtrConstRef);
 		}
 
 		R								operator()(const T1& oT1)
@@ -167,65 +208,12 @@ public:
 			return R();
 		}
 	protected:
-		Caller*							m_pCaller;
+		CallerOneArg<T1, R>*			m_pCaller;
 	};
 
 	template<typename T1, typename T2, typename R>
 	class CallbackTwoArg
 	{
-		class Caller
-		{
-		public:
-			virtual R					Call(const T1& oT1, const T2& oT2) = 0;
-		};
-
-		class FunctionCall : public Caller
-		{
-		public:
-			FunctionCall(R(*pFunctionPtr)(T1, T2))
-			{
-				m_pFunctionPtr = pFunctionPtr;
-			}
-			virtual R					Call(const T1& oT1, const T2& oT2)
-			{
-				return m_pFunctionPtr(oT1, oT2);
-			}
-			R(*m_pFunctionPtr)(T1, T2);
-		};
-
-		template<typename C>
-		class InstanceCall : public Caller
-		{
-		public:
-			InstanceCall(C* pThis, R(C::*pMemberPtr)(T1, T2))
-			{
-				m_pThis = pThis;
-				m_pMemberPtr = pMemberPtr;
-			}
-			virtual R					Call(const T1& oT1, const T2& oT2)
-			{
-				return (m_pThis->*m_pMemberPtr)(oT1, oT2);
-			}
-			R(C::*m_pMemberPtr)(T1, T2);
-			C* m_pThis;
-		};
-
-		template<typename C>
-		class InstanceCallConstRef : public Caller
-		{
-		public:
-			InstanceCallConstRef(C* pThis, R(C::*pMemberPtr)(T1, T2))
-			{
-				m_pThis = pThis;
-				m_pMemberPtr = pMemberPtr;
-			}
-			virtual R					Call(const T1& oT1, const T2& oT2)
-			{
-				return (m_pThis->*m_pMemberPtr)(oT1, oT2);
-			}
-			R(C::*m_pMemberPtr)(T1, T2);
-			C* m_pThis;
-		};
 	public:
 		CallbackTwoArg()
 		{
@@ -238,21 +226,14 @@ public:
 		void							Set(R(*pFuncPtr)(T1, T2))
 		{
 			if (0 != m_pCaller) delete m_pCaller;
-			m_pCaller = new FunctionCall(pFuncPtr);
+			m_pCaller = new FunctionCaller(pFuncPtr);
 		}
 
 		template<typename C>
 		void							Set(C* pThis, R(C::*pMemberPtr)(T1, T2))
 		{
 			if (NULL != m_pCaller) delete m_pCaller;
-			m_pCaller = new InstanceCall<C>(pThis, pMemberPtr);
-		}
-
-		template<typename C>
-		void							Set(C* pThis, R(C::*pMemberPtr)(const T1&, const T2&))
-		{
-			if (NULL != m_pCaller) delete m_pCaller;
-			m_pCaller = new InstanceCallConstRef<C>(pThis, pMemberPtr);
+			m_pCaller = new InstanceCallerTwoArg<C, T1, T2, R>(pThis, pMemberPtr);
 		}
 
 		R								operator()(const T1& oT1, const T2& oT2)
@@ -261,7 +242,7 @@ public:
 			return R();
 		}
 	protected:
-		Caller*	m_pCaller;
+		CallerTwoArg<T1, T2, R>*		m_pCaller;
 	};
 
 	// Enums and structs
